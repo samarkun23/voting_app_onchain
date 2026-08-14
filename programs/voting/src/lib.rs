1f33f0156bcdc1fbb9b33033932c2126a87ab9f2
samarkun <samarkun4@gmail.com>
2026-08-14 21:24:47 +0530

    create a accounts and pass it to the context

--- programs/voting/src/lib.rs
+++ programs/voting/src/lib.rs
@@ -1,4 +1,4 @@
-use anchor_lang::prelude::*;
+use anchor_lang::prelude::{borsh::de, *};
 
 declare_id!("JBmRNtAuk4adsXxhQaSj1LVCPBj1pWRpnX1E1D1tQcm5");
 
@@ -6,18 +6,38 @@ declare_id!("JBmRNtAuk4adsXxhQaSj1LVCPBj1pWRpnX1E1D1tQcm5");
 pub mod voting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn init_poll(ctx: Context<InitPoll>, _poll_id: u64, start: u64, end: u64, name: String, description: String) -> Result<()> {
+        let mut pool = &mut ctx.accounts.poll_account;
+        pool.poll_name = name;
+        pool.poll_description = description;
+        pool.poll_voting_start = start;
+        pool.poll_voting_end = end;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(poll_id: u64)]
+pub struct InitPoll<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + PollAccount::INIT_SPACE,
+        seeds = [b"pool".as_ref(), poll_id.to_le_bytes().as_ref()],
+        bump
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+
+    pub system_program: Program<'info, System>
+}
+
 
 #[account]
 #[derive(InitSpace)]
-pub struct VotingAccount {
+pub struct PollAccount {
     #[max_len(32)]
     pub poll_name : String,
     #[max_len(280)]
@@ -27,3 +47,10 @@ pub struct VotingAccount {
     pub poll_options_index: u64,
 }
 
+#[account]
+#[derive(InitSpace)]
+pub struct CandidateAccount {
+    #[max_len(32)]
+    pub candidate_name : String,
+    pub candidate_votes: u64,
+}
